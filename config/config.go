3a1cdc744487e09d98dc2f06bc154f7cc86cb747
lakerszhy <lakerszhy@gmail.com>
2026-05-21 17:27:25 +0800

    simplify log file path resolution using XDG StateFile directly

--- config/config.go
+++ config/config.go
@@ -1,9 +1,7 @@
 package config
 
 import (
-	"errors"
 	"io"
-	"io/fs"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -16,18 +14,11 @@ const (
 )
 
 func InitLogger() (io.Closer, error) {
-	dir, err := xdg.StateFile(AppName)
+	filePath, err := xdg.StateFile(filepath.Join(AppName, AppName+".log"))
 	if err != nil {
 		return nil, err
 	}
 
-	if _, err = os.Stat(dir); errors.Is(err, fs.ErrNotExist) {
-		if err = os.MkdirAll(dir, 0700); err != nil {
-			return nil, err
-		}
-	}
-
-	filePath := filepath.Join(dir, AppName+".log")
 	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 	if err != nil {
 		return nil, err
